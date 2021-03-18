e5311d675f75b5d9676546db567ff82e90fcf83a
Eddie Flores <mreddief1989@gmail.com>
2021-03-18 16:26:55 -0500

    commit tree 82fe8cd51ea1617b5efe9035f8b0f4565cf6fa71

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Code your solution in this file!
-let companyName = 'Scuber';
-const = 'Scuber';
-const mostProfitableNeighborhood = 'Chelsea';
-let  = 'Chelsea';
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = 'Susan Smith';
