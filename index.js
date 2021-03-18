fc76e23a0c6b6cc7dc3c0ec34b1b2f8a9303ea4a
Eddie Flores <mreddief1989@gmail.com>
2021-03-18 16:00:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+const = 'Scuber';
+const mostProfitableNeighborhood = 'Chelsea';
+let  = 'Chelsea';
+let companyCeo = 'Susan Smith';
